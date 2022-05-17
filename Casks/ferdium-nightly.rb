c1ad94752ddd32bd2e493fb9267e92cd7c9fa972
Vijay A <vraravam@users.noreply.github.com>
2022-05-16 21:49:48 -0500

    Update for ferdium-nightly '6.0.0-nightly.38'

--- Casks/ferdium-nightly.rb
+++ Casks/ferdium-nightly.rb
@@ -1,12 +1,12 @@
 cask "ferdium-nightly" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "6.0.0-nightly.37"
+  version "6.0.0-nightly.38"
 
   if Hardware::CPU.intel?
-    sha256 "ca7e1be37075e0399124af4f1de2c182f3879eaff69aa729b7f9fa64fe4c68d4"
+    sha256 "83eb45d742791b674329450853fe82dad8983fcf27eaf7e9d8dadff14e7e2262"
   else
-    sha256 "11c9a3451fc68f88f4f340936ccb4d3b57612eb6b809eeb298e02f881f663022"
+    sha256 "4915597e7f0b03eb968c1378e1dd876ef2847feba86c2fff58027fc22e770cd5"
   end
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-#{version}#{arch}.dmg",
