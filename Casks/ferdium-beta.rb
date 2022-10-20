a4d482040d5e03cc33683fb8e16ef30e7f009fee
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2022-10-20 09:11:39 +0530

    Update ferdium-beta from 6.2.0 to 6.2.1-beta.1

--- Casks/ferdium-beta.rb
+++ Casks/ferdium-beta.rb
@@ -1,20 +1,16 @@
 cask "ferdium-beta" do
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  arch arm: "arm64", intel: "x64"
 
-  version "6.0.0-beta.3"
-
-  if Hardware::CPU.intel?
-    sha256 "72805acb7970ca2ff1f99e4c85a7f65745dbbae2e0441ee6bc6e29052f5bbcb3"
-  else
-    sha256 "3923c2b264c664393f4f4f0a231769a9a6f00ba4be5fa0073edf30b7e0d44d98"
-  end
+  version "6.2.1-beta.1"
+  sha256 arm:   "2d6b9ac4b948e4f90ce2db99a19c181237f7a865ecce7749b2882f71b3928a4b",
+         intel: "88cf90ed421d50571ff057ae53851ea9a7ea58751494626e333d062bbc3c3a6d"
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
       verified: "github.com/ferdium/ferdium-app/"
-
   name "Ferdium"
   desc "All your services in one place"
   homepage "https://ferdium.org/"
+
   conflicts_with cask: "ferdium-nightly"
 
   # TODO: Need to turn this on after the in-app updater is verified to work
