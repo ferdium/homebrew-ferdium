7b11eaeec947cbf194a7b420a4b0014da7f54549
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2022-10-29 03:32:30 +0000

    Update ferdium-nightly from 6.2.1-nightly.31 to 6.2.1-nightly.33

--- Casks/ferdium-nightly.rb
+++ Casks/ferdium-nightly.rb
@@ -1,9 +1,9 @@
 cask "ferdium-nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.2.1-nightly.31"
-  sha256 arm:   "3f5597594d770fd697d755678b6cc7a60d94475f8a3651d14da10894c0fcf881",
-         intel: "cd015bba8dd6fc3f1876fc71155a09e069c81abb8e1a1f0a11bb7bd34efbbbc7"
+  version "6.2.1-nightly.33"
+  sha256 arm:   "3a65f7dd19ae65dd56f5dc6dcf87b314df7010da166ac451c29d4331a97403e0",
+         intel: "1d4417c8c36d72183d4604029a5b46f7ef39d38c2c6a8d64a281477eaaa7707d"
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
       verified: "github.com/ferdium/ferdium-app/"
