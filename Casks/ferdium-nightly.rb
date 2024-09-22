655c75df48761e58f72e016012bc43f61417748e
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2024-09-22 04:06:56 +0000

    ferdium-nightly 6.7.7-nightly.8

--- Casks/ferdium-nightly.rb
+++ Casks/ferdium-nightly.rb
@@ -1,9 +1,9 @@
 cask "ferdium-nightly" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.7.7-nightly.7"
-  sha256 arm:   "948b79f78864a90e92800eccfa2cecb95a4f9f42cfa241e5670acc8ffe0995cc",
-         intel: "2f2f6b0fb9b28ba4e3c841740f559cbca3193fb71dffd9b45c2c0c44b1ae05d7"
+  version "6.7.7-nightly.8"
+  sha256 arm:   "f59a38ca96d1f53de74e3e342246ef4cca04bb2812ee87ac752ca70e0be178bf",
+         intel: "ad947dd9981fba0f80129ac95bce9f45ffa98b70981cc07b5009dd942cb403c4"
 
   url "https://github.com/ferdium/ferdium-app/releases/download/v#{version}/Ferdium-mac-#{version}-#{arch}.dmg",
       verified: "github.com/ferdium/ferdium-app/"
